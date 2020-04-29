48a387b7989a0d6a9154fb32d1afca628f8f3da1
Vincent <fritsch_vincent@hotmail.fr>
2020-04-29 19:41:21 +0200

    Created form category & country

--- public/template-parts/home.php
+++ public/template-parts/home.php
@@ -4,7 +4,7 @@ $webcam = new \App\WebcamApi();
 $countries = $webcam->getCountries();
 $categories = $webcam->getCategories();
 
-var_dump($countries);
+
 
 include './template-parts/header.php';
 include './template-parts/index-parts/index-content.php';
---
+++ public/template-parts/travel-parts/travel-form.php
@@ -0,0 +1,30 @@
+<?php
+$webcam = new \App\WebcamApi();
+$countries = $webcam->getCountries();
+$categories = $webcam->getCategories();
+
+?>
+
+<form method="post" >
+        <div class="form-group ">
+            <label for="exampleFormControlSelect2">Select a category
+             <select name="category"  class="form-control" id="">
+             <option>Select your category </option>
+            <?php foreach($categories as $key => $val ){ ?>
+            <option value="<?= $key ?>"><?= $key ?></option>
+            <?php } ?>
+            </select></label>
+         </div>
+
+        <div class="form-group ">
+            <label for="">Select a country
+                <select name="$key"  class="form-control" id="">
+                    <option>Select your country </option>
+                <?php foreach($countries as $key => $val ){ ?>
+            <option value="<?= $key ?>"><?= $val ?></option>
+            <?php } ?>
+            </select> </label>
+         </div>
+    <input type="submit" value="Send">
+
+</form>
