0bbe87bfbbb1b510f063ea0859c3c21583230c6d
Moxymore67 <mark.alexandre.strasbourg@gmail.com>
2020-04-29 10:53:55 +0200

    update api key

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
 
+/**
+ * Reminders for API keys
+ *
+ * Point Forecast : KzIAJvoO7fv0IQboxiNsAf5dgkZYuZ5C
+ * Webcam : CSueTiJgLo8WgS54Jc8c5xZX6QX5I8jv
+ * Map : BEJn4Y9vLjc0xRQwTh3g8dd3UEdzZXRW
+ */
+
 // Autoload
+phpinfo();
 require '../vendor/autoload.php';
