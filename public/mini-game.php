33b8dd6ea482791fb7fc4320717dc363f61d271d
Moxymore67 <mark.alexandre.strasbourg@gmail.com>
2020-04-29 19:24:40 +0200

    more pages

---
+++ public/mini-game.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Reminders for API keys
+ *
+ * Point Forecast : KzIAJvoO7fv0IQboxiNsAf5dgkZYuZ5C
+ * Webcam : CSueTiJgLo8WgS54Jc8c5xZX6QX5I8jv
+ * Map : BEJn4Y9vLjc0xRQwTh3g8dd3UEdzZXRW
+ */
+
+require_once '../vendor/autoload.php';
+
+require_once 'template-parts/mini-game.php';
