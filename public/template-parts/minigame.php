2e1e803f530183833cd8a752cbbe20e0fbb99800
Pierre Muller <pierremanu.muller@gmail.com>
2020-04-29 22:38:42 +0200

    added comments

--- public/template-parts/minigame.php
+++ public/template-parts/minigame.php
@@ -5,18 +5,27 @@ session_start();
 $webcam = new \App\WebcamApi();
 $countries = $webcam->getCountries();
 
-
+// MINI GAME LOGIC
+// On page first visit or reload
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // new RandomCountry object
     $countryGenerator = new \App\RandomCountry();
+    // pick a random country
     $randCountry = $countryGenerator->getRandomCountry();
+    // Storing it into SESSION
     $_SESSION['country'] = $randCountry;
     var_dump($randCountry);
+// On guessing a country
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // checking not a blank guess
     if ($_POST['guess'] == 'none') {
+        // If so, redirect
         header('location: minigame.php');
+    // If valid guess
     } else {
+        // Storing guess into SESSION
         $_SESSION['guess'] = $_POST['guess'];
-        $message = '';
+        // Compare the results
         if ($_SESSION['country'] == $_SESSION['guess']) {
             $message = "You're the best !";
         } else {
